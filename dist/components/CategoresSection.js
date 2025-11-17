166c7cb0993d98567ad216ca373586683a3126c2
Vyshnav S <vyshnav@Vyshnavs-Air.bbrouter>
2025-11-17 18:06:57 +0530

    Replaced the categories section css name in import

--- dist/components/CategoresSection.js
+++ dist/components/CategoresSection.js
@@ -1,6 +1,6 @@
 'use client';
 import { jsx as _jsx } from "react/jsx-runtime";
-import styles from './categories_section.module.css';
+import styles from './CategoriesSection.module.css';
 export default function CategoriesSection({ categories, selectedCategory, onCategoryChange, className = '', }) {
     return (_jsx("div", { className: `${styles.categoriesSection} ${className}`, children: _jsx("div", { className: styles.categoriesContainer, children: categories.map((category) => (_jsx("button", { className: `${styles.categoryBtn} ${selectedCategory === category ? styles.active : ''}`, onClick: () => onCategoryChange(category), children: category }, category))) }) }));
 }
